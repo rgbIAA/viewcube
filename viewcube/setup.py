a540da6816a7cdda8dcddf851d6c3ac388911cbe
Pascual Pertegal <pasperrui@alu.edu.gva.es>
2025-05-07 18:51:47 +0200

    build: updated setup.py with package version from pypi
    Took 10 minutes

--- viewcube/setup.py
+++ viewcube/setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mi_paquete',
-    version='0.1',
+    name='viewcube',
+    version='0.2.9',
     packages=find_packages(),
     install_requires=[
         'matplotlib',
