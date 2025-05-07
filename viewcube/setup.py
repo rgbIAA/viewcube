bead4718b412e1b2ff664fdef3ccc7d475f5f28d
Pascual Pertegal <pasperrui@alu.edu.gva.es>
2025-05-07 18:40:52 +0200

    build: add setup.py with package configuration and dependencies
    Took 6 minutes

---
+++ viewcube/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mi_paquete',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'matplotlib',
+        'setuptools',
+    ],
+)
